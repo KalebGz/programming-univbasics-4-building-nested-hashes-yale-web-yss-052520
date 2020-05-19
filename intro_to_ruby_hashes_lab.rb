9a571acd4ee6a09183b46146247701e82614d2cc
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-19 07:44:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,14 +15,13 @@ def monopoly_with_second_tier
   # complexity.
 
 new_hash = {
-  railroads: {}
-}
-base_hash[:railroads] = {
+  railroads: {
   pieces: 4,
   rent_in_dollars: {},
   names: {}
+  }
 }
-new_hash
+
 end
 
 def monopoly_with_third_tier
